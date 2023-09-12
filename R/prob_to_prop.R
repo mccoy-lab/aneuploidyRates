39ed4370356efd6235ba77eef7baefb2f7d614ea
Nemo <qyang40@jh.edu>
2023-09-12 14:29:40 -0400

    Changed error messages and comments according to pull request comments

--- R/prob_to_prop.R
+++ R/prob_to_prop.R
@@ -13,17 +13,30 @@
 
 prob_to_prop <- function(prob.meio, prob.mito, num.division = 8) {
   # Error messages
-  if(prob.meio < 0 | prob.mito < 0){
-    stop(paste0("The probabilities: ", prob.meio, ", ",
-                prob.mito, " must be greater than 0"))
+  if (prob.meio < 0 | prob.mito < 0) {
+    stop(paste0(
+      "The probabilities: ",
+      prob.meio,
+      ", ",
+      prob.mito,
+      " must be at least 0"
+    ))
   }
-  if(prob.meio > 1 | prob.mito > 1){
-    stop(paste0("The probabilities: ", prob.meio, ", ",
-                prob.mito, " must be less than 1"))
+  if (prob.meio > 1 | prob.mito > 1) {
+    stop(paste0(
+      "The probabilities: ",
+      prob.meio,
+      ", ",
+      prob.mito,
+      " must be at most 1"
+    ))
   }
-  if(num.division %% 1 != 0){
-    stop(paste0("The number of cell division: ", 
-                num.division, "should be an integer"))
+  if (num.division %% 1 != 0) {
+    stop(paste0(
+      "The number of cell division: ",
+      num.division,
+      "should be an integer"
+    ))
   }
   
   cells.affected <- 0
@@ -32,6 +45,8 @@ prob_to_prop <- function(prob.meio, prob.mito, num.division = 8) {
   # Meiotic check:
   has.meio.error <- runif(1) < prob.meio
   if (has.meio.error) {
+    # Meiotic-errored embryos always have fully aneuploidy.
+    # Thus, prop.aneu = 1
     return(1)
   } else{
     # Mitotic errors
@@ -46,7 +61,9 @@ prob_to_prop <- function(prob.meio, prob.mito, num.division = 8) {
 
 #' Simulate cell division and count the number of affected (aneuploid) cells.
 #' Recursive.
-#' Don't keep track of aneuploidies: just stop and do math.
+#' The derived cells of an aneuploid cell will all be aneuploids. Therefore,
+#' we can tally the number of affected cells mathematically.
+#'
 #' @param cell.affected current total number of affected cells
 #' @param n.division number of divisions simulated
 #' @param prob.affected the likelihood of a cell to be an aneuploid.
@@ -59,26 +76,39 @@ mito_aneu_cells <- function(cells.affected = 0,
                             prob.affected = 0.5,
                             currently.affected = F) {
   # Error messages
-  if(prob.affected < 0){
-    stop(paste0("The probability of being affected: ",
-                prob.affected, " must be greater than 0"))
+  if (prob.affected < 0) {
+    stop(paste0(
+      "The probability of being affected: ",
+      prob.affected,
+      " must be at least 0"
+    ))
   }
-  if(prob.affected > 1){
-    stop(paste0("The probability of being affected: ",
-                prob.affected, " must be less than 1"))
+  if (prob.affected > 1) {
+    stop(paste0(
+      "The probability of being affected: ",
+      prob.affected,
+      " must be at most 1"
+    ))
   }
-  if(cells.affected %% 1 != 0){
-    stop(paste0("The number of cells affected: ", 
-                cells.affected, "should be an integer"))
+  if (cells.affected %% 1 != 0) {
+    stop(paste0(
+      "The number of cells affected: ",
+      cells.affected,
+      "should be an integer"
+    ))
   }
-  if(n.division %% 1 != 0){
-    stop(paste0("The number of cell division: ", 
-                n.division, "should be an integer"))
+  if (n.division %% 1 != 0) {
+    stop(paste0(
+      "The number of cell division: ",
+      n.division,
+      "should be an integer"
+    ))
   }
   
-  # Short cut
+  # Short cut: since every cell derived from an affected cell
+  # is also affected, we can just calculate and return the tally.
   if (currently.affected) {
-    # stop and add all its children cells to cells affected
+    # add all its children cells to cells affected
     cells.affected <- cells.affected + (2 ^ n.division)
     return(cells.affected)
   }
@@ -97,12 +127,12 @@ mito_aneu_cells <- function(cells.affected = 0,
           currently.affected = T
         )
       } else{
-        cells.affected <-  mito_aneu_cells(
-          cells.affected = cells.affected,
-          n.division = n.division - 1,
-          prob.affected = prob.affected,
-          currently.affected = currently.affected
-        )
+          cells.affected <-  mito_aneu_cells(
+            cells.affected = cells.affected,
+            n.division = n.division - 1,
+            prob.affected = prob.affected,
+            currently.affected = currently.affected
+          )
       }
     }
   }
