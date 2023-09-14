af7550dabc6068ebc859a7deca43e5143799aa34
Nemo <qyang40@jh.edu>
2023-09-14 13:49:21 -0400

    Completed take_biopsy error messages and tests, passed all

--- tests/testthat/test-take_biopsy.R
+++ tests/testthat/test-take_biopsy.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+em = Embryo()
+test_that("Error message works",{
+  expect_error(take_biopsy(1))
+  expect_error(take_biopsy(em, biop.size = 1000))
+  expect_error(take_biopsy(em, biop.size = 0))
+})
+
+test_that("Biopsy types are correctly identified", {
+  expect_equal(take_biopsy(Embryo(prop.aneuploid = 1)), 2)
+  expect_equal(take_biopsy(Embryo(prop.aneuploid = 0)), 0)
+  expect_equal(take_biopsy(Embryo(prop.aneuploid = 1), biop.size = 1), 2)
 })
