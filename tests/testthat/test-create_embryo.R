aa1b76e168566d66e153b2a26d4888f6b4b526a4
Nemo <qyang40@jh.edu>
2023-09-13 21:00:57 -0400

    Changed method name from Embryo to create_embryo, passed tests

--- tests/testthat/test-create_embryo.R
+++ tests/testthat/test-create_embryo.R
@@ -24,7 +24,7 @@ test_that("Embryo has aneuploid specified chromosomes", {
       chrs = c(rep(0, 30), 1)
     }
 
-    e = Embryo(n.cells = 100, n.chrs = 31, prop.aneuploid = chrs, dispersal = 0)
+    e = create_embryo(n.cells = 100, n.chrs = 31, prop.aneuploid = chrs, dispersal = 0)
     expect_equal(tessera::countAneuploidCells(e, i), 100)
   }
 
@@ -33,7 +33,7 @@ test_that("Embryo has aneuploid specified chromosomes", {
 test_that("Number of aneuploid cells matches input aneuploidy level", {
 
   for(i in seq(0, 1, 0.05)){
-    e = Embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = i, dispersal = 0)
+    e = create_embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = i, dispersal = 0)
     expect_equal(tessera::countAneuploidCells(e), i*100)
   }
 
@@ -43,12 +43,12 @@ test_that("Number of aneuploid cells matches input aneuploidy level", {
 test_that("RNG seed makes reproducible embryos", {
 
   # Embryos with the same seed should be strictly identical
-  e = Embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = 0.5, dispersal = 0, rng.seed = 42)
-  e2 = Embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = 0.5, dispersal = 0, rng.seed = 42)
+  e = create_embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = 0.5, dispersal = 0, rng.seed = 42)
+  e2 = create_embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = 0.5, dispersal = 0, rng.seed = 42)
   expect_identical(e, e2)
 
   # Embryos with different seeds should be different
-  e3 =Embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = 0.5, dispersal = 0, rng.seed = 43)
+  e3 = create_embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = 0.5, dispersal = 0, rng.seed = 43)
   expect_false(isTRUE(all.equal(e, e3)))
 })
 
@@ -62,7 +62,7 @@ test_that("Clustered embryos have all aneuploid cells adjacent to another", {
 
   for(aneu in seq(0.05, 0.95, 0.05)){
     # clustered embryo
-    e = Embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = aneu, dispersal = 0)
+    e = create_embryo(n.cells = 100, n.chrs = 1, prop.aneuploid = aneu, dispersal = 0)
 
     # Check any aneuploid cells have an aneuploid neighbour
     for(i in 1:100){
