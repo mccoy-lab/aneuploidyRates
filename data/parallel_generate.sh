a7ed4eba79c3b9eb36f7e3ad3db4a7b816647b7e
YNemokun <qyang40@jh.edu>
2023-10-09 19:39:28 -0400

    Attempted shell script for parallel jobs

---
+++ data/parallel_generate.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#SBATCH --job-name="aneuploidy_rates_run"
+#SBATCH --output="rates_file"
+#SBATCH --partition=defq
+#SBATCH --t 00-00:20:00
+#SBATCH --nodes-2
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=100G
+#SBATCH --mail-type=end
+#SBATCH --mail-user=qyang40@jhu.edu
+#SBATCH --array=1-2%2
+#SBATCH --account=rmccoy22
+
+module load r
+
+export basisdir=/home/qyang40/scratch16-rmccoy22/qyang40/aneuploidyRates
+export workdir=/home/qyang40/scratch16-rmccoy22/qyang40/aneuploidyRates
+
+echo
+Rscript ${basisdir}/R/find_rates.R > $workdir/data/a.out
