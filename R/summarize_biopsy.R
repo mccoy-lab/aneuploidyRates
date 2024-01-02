4196e6ff597cf4ce4c93de9a2ede4d8907409c2b
Nemo <qyang40@jh.edu>
2024-01-02 15:53:03 -0500

    Changed dataframes to lists and matrices, need to compare find_rates

--- R/summarize_biopsy.R
+++ R/summarize_biopsy.R
@@ -1,9 +1,9 @@
 # This file returns a data frame of a summary of multiple trials
 # (Will this file be doing too much jobs by calling all the previous functions?)
 
-# source("R/create_embryo.R")
-# source("R/prob_to_prop.R")
-# source("R/take_biopsy.R")
+source("R/create_embryo.R")
+source("R/prob_to_prop.R")
+source("R/take_biopsy.R")
 
 #' A wrapper function for generating the biopsy results
 #' @param num.em the number of embryos to be created
@@ -52,37 +52,28 @@ summarize_biopsy <- function(num.em = 100,
 
   # set up result file
   # without the default settings
-  if (hide.default.param) {
-    result <- data.frame(
-      prop.aneu = 0,
-      prob.meio = meio,
-      prob.mito = mito,
-      dispersal = 0,
-      euploid = 0,
-      mosaic = 0,
-      aneuploid = 0
-    )
-  } else{
-    # include all the constant parameters to set up the embryo
-    result <- data.frame(
-      prop.aneu = 0,
-      prob.meio = meio,
-      prob.mito = mito,
-      dispersal = 0,
-      euploid = 0,
-      mosaic = 0,
-      aneuploid = 0,
-      num.cell = 0,
-      num.chr = 0,
-      concordance = 0
-    )
+  # result <- data.frame(
+  #   prop.aneu = 0,
+  #   prob.meio = meio,
+  #   prob.mito = mito,
+  #   dispersal = 0,
+  #   euploid = 0,
+  #   mosaic = 0,
+  #   aneuploid = 0
+  # )
+  result <- c(0, meio, mito, dispersal, 0, 0, 0)
+  if (!hide.default.param) {
+    result <- c(result, num.cell, num.chr, concordance)
   }
+  # print(result)
+
   # run embryos
   for (i in 1:num.em) {
     # convert to prop.aneu
     prop.aneu <- prob_to_prop(prob.meio = meio, prob.mito = mito)
     # store the sum of all prop.aneus (will take the average later)
-    result$prop.aneu <- result$prop.aneu + prop.aneu
+    result[1] <- result[1] + prop.aneu
+
     # create an embryo
     em <- create_embryo(
       prop.aneuploid = prop.aneu,
@@ -91,45 +82,35 @@ summarize_biopsy <- function(num.em = 100,
       dispersal = dispersal,
       concordance = concordance
     )
+
     # take biopsy
     type <- take_biopsy(em)
+
     # add to result
     if (type == 0) {
-      result$euploid <- result$euploid + 1
+      result[5] <- result[5] + 1
 
     } else if (type == 1) {
-      result$mosaic <- result$mosaic + 1
+      result[6] <- result[6] + 1
 
     } else{
-      result$aneuploid <- result$aneuploid + 1
+      result[7] <- result[7] + 1
 
     }
   }
 
-  # # calculate the average prop.aneu and convert the types to percentages
-  # result <- cbind(result$prop.aneu/num.em, result[,2:3], result[, 4:6] / num.em)
-  # # if needed: store the underlying parameters
-  # if(!hide.default.param){
-  #   result <- cbind(result, num.cell, num.chr, dispersal, concordance)
-  # }
-
   # Add dispersal as a potential parameter
   # calculate the average prop.aneu and convert the types to percentages
-  prop.aneu <- result$prop.aneu / num.em
-  result <-
-    cbind(prop.aneu, result[, 2:3], dispersal, result[, 5:7] / num.em)
-  # if needed: store the underlying parameters
-  if (!hide.default.param) {
-    result <- cbind(result, num.cell, num.chr, concordance)
-  }
+  result[c(1, 5:7)] <- result[c(1, 5:7)] / num.em
   return(result)
 }
-#
-result = summarize_biopsy(
+
+result <- summarize_biopsy(
   meio = 0,
   mito = 0.3,
   dispersal = 0.5,
   hide.default.param = TRUE
 )
-print(result[1, 4:6] == list(0, 0, 1))
-print(result)
+# print(result[c(1, 5:7)] == list(1, 0, 0, 1))
+# print(result)
+# print(typeof(result))
--- data/generate_data.R
+++ data/generate_data.R
@@ -4,4 +4,12 @@
 source("R/find_rates.R")
 
 # find_rates(num.trials = 1000, tolerance = 0.01)
-find_rates(num.trials = 50)
+# system.time(
+#   df <- find_rates_faster(num.trials = 50)
+# )
+# print(typeof(df))
+# print(df)
+
+df1 <- find_rates_faster(num.trials = 50)
+df2 <- find_rates(num.trials = 50)
+microbenchmark(df1, df2, times = 5)
