2ed83d0b6fe9ea1e99d78096cd83dc6045d711d1
Nemo <qyang40@jh.edu>
2024-01-03 11:44:31 -0500

    Checked and kept faster find_rates function with matrix

--- data/generate_data.R
+++ data/generate_data.R
@@ -2,14 +2,22 @@
 #' the generated data to a csv file
 #'
 source("R/find_rates.R")
+library(microbenchmark)
+
 
 # find_rates(num.trials = 1000, tolerance = 0.01)
+
+# Performance check
 # system.time(
 #   df <- find_rates_faster(num.trials = 50)
 # )
 # print(typeof(df))
 # print(df)
 
-df1 <- find_rates_faster(num.trials = 50)
-df2 <- find_rates(num.trials = 50)
-microbenchmark(df1, df2, times = 5)
+# system.time(
+#   df1 <- find_rates_faster(num.trials = 50)
+# )
+# system.time(
+#   df2 <- find_rates(num.trials = 50)
+# )
+# microbenchmark(df1, df2, times = 10000)
