44253cd92e6f457dd604f67d586abbed8b500687
Angela <qyang40@jh.edu>
2024-01-12 11:53:53 -0500

    Moved data to inst folder, successfully ignored and built

---
+++ inst/data/generate_data.R
@@ -0,0 +1,18 @@
+# This file runs the aneuploidyRates package and writes the generated data to a csv file
+#
+# source("R/find_rates.R")
+if(!require(aneuploidyRates)){
+  if(!require(devtools)) install.packages("devtools")
+  library(devtools)
+  install_github("mccoy-lab/aneuploidyRates")
+}
+library(aneuploidyRates)
+find_rates(num.trials = 1000, tolerance = 0.01)
+# find_rates(num.trials = 50)
+
+# Performance check
+# system.time(
+#   df <- find_rates(num.trials = 50)
+# )
+# print(typeof(df))
+# print(df)
