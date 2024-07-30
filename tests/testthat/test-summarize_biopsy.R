ee5a4054ff9db6c0d5903edc830c88799a1065da
Angela <qyang40@jh.edu>
2024-07-30 12:47:06 -0400

    Updated tests in summarize_biopsy according to the prop.aneu preservation

--- tests/testthat/test-summarize_biopsy.R
+++ tests/testthat/test-summarize_biopsy.R
@@ -23,16 +23,19 @@ sample2 <- c(
    1, 1, 0, 0, 0, 0, 1, 200, 1, 0
 )
 test.df <- summarize_biopsy(meio = 0.2, mito = 0.01)
+test.df2 <- summarize_biopsy(num.em = 10, 1, 0)
 test_that("Code returns desired output", {
-  expect_equal(summarize_biopsy(num.em = 10, 1, 0), sample)
-  expect_equal(summarize_biopsy(num.em = 10, 1, 0, hide.default.param = FALSE),
+  expect_equal(dim(test.df2), c(10, 7))
+  expect_equal(test.df2[1,], sample)
+  expect_equal(summarize_biopsy(num.em = 10, 1, 0, hide.default.param = FALSE)[1,],
                sample2)
-  expect_true(test.df[5] > test.df[7])
-  expect_true(test.df[5] > test.df[6])
+  expect_true(test.df[1, 5] > test.df[1, 7])
+  expect_true(test.df[1, 5] > test.df[1, 6])
 })
 
-test.df2 <- summarize_biopsy(meio = 0.4, mito = 0.5)
+test.df3 <- summarize_biopsy(meio = 0.4, mito = 0.5)
 test_that("Code returns desired output", {
-  expect_true(test.df2[5] < test.df2[7])
-  expect_true(test.df2[5] <= test.df2[6])
+  expect_equal(dim(test.df3), c(100,7))
+  expect_true(test.df3[1, 5] < test.df3[1, 7])
+  expect_true(test.df3[1, 5] <= test.df3[1, 6])
 })
