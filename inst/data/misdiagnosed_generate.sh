a287b19354f909fc3b9bc433d0230478476bdfbf
Angela <qyang40@jh.edu>
2024-08-20 00:54:35 -0400

    Edited script to run mis-calculated rates

--- inst/data/misdiagnosed_generate.sh
+++ inst/data/misdiagnosed_generate.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --job-name="aneuploidy_rates_run"
 #SBATCH --partition=parallel
-#SBATCH --time 00-08:30:00
+#SBATCH --time 00-60:00:00
 #SBATCH --mem=100G
 #SBATCH --mail-type=end
 #SBATCH --mail-user=qyang40@jhu.edu
@@ -19,7 +19,7 @@ conda activate ~/miniconda3/envs/aneuploidy_rates
 filepath=$(pwd)
 
 now=$(date +"%Y-%m-%d")
-outdir="${now}c"
+outdir="${now}_${SLURM_ARRAY_TASK_ID}"
 mkdir -p $outdir
 
 export basedir=${filepath}
@@ -28,5 +28,5 @@ export basedir=${filepath}
 echo ${basedir}
 echo ${SLURM_ARRAY_TASK_ID}
 # Rscript ${basedir}/misdiagnosed_rates.R ${SLURM_ARRAY_TASK_ID}> $basedir/$outdir/${SLURM_ARRAY_TASK_ID}.csv
-output_file="${basedir}/${outdir}/${SLURM_ARRAY_TASK_ID}.csv"
-Rscript ${basedir}/misdiagnosed_rates.R ${SLURM_ARRAY_TASK_ID} $output_file
+# output_file="${basedir}/${outdir}/${SLURM_ARRAY_TASK_ID}.csv"
+Rscript ${basedir}/misdiagnosed_rates.R ${SLURM_ARRAY_TASK_ID} "${basedir}/${outdir}/full_data.csv" "${basedir}/${outdir}/data.csv"
